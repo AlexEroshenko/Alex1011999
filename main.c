146c79963aeb35dd01381d6d99f4371f3622a386
alex <eroshenko-99@mail.ru>
2018-04-27 03:12:45 +0300

    "difference"

--- main.c
+++ main.c
@@ -5,5 +5,6 @@ int main(void)
     int a,b;
     printf("input 2 whole number");
     scanf("%d%d",&a&b);
-    printf("a+b=%d+%d",a,b);
+    printf("a+b=%d+%d",a,b,a+b);
+    printf("a-b=%d+%d",a,b,a-b);
 }
